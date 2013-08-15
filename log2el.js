3a09cade2cfb2f0f23222e4daeb4b32886068df5
Jon Davis <jon@jondavis.net>
2013-08-15 07:23:35 -0700

    Update log2el.js

--- log2el.js
+++ log2el.js
@@ -5,13 +5,19 @@
         n=n.toString();
         while (n.length < w) n = c + n;
         return n;
-    };
-    global.log2el = function(s) {
-        for (var i=1; i<arguments.length; i++) {
-            s = s.replace(new RegExp("\\{" + (i-1) + "\\}", 'g'), arguments[i]);
-        }
-        var el = $('<div><pre></pre></div>');
-        el.find('pre').text(strprepad(++ilog,2, " ") + ': ' + s);
-        $('#log').append(el);
+    }
+    global.log2el = function(targetel, s) {
+      debugger;
+      var args = $.makeArray(arguments);
+      if (typeof(targetel) == 'string') {
+        args.unshift($('#log').length > 0 ? $('#log') : $('body'));
+      }
+      targetel = args[0];
+      for (var i=1; i<args.length; i++) {
+          s = s.replace(new RegExp("\\{" + (i-1) + "\\}", 'g'), args[i]);
+      }
+      var el = $('<div><pre></pre></div>');
+      el.find('pre').text(strprepad(++ilog,2, " ") + ': ' + s);
+      $(targetel).append(el);
     };
 })();

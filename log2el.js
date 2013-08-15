84319b19b472735d920b96e4d40dc825ece7adca
Jon Davis <jon@jondavis.net>
2013-08-15 07:35:04 -0700

    Update log2el.js

--- log2el.js
+++ log2el.js
@@ -16,8 +16,8 @@
       for (var i=2; i<args.length; i++) {
           s = s.replace(new RegExp("\\{" + (i-2) + "\\}", 'g'), args[i]);
       }
-      var el = $('<div><pre class="log-item"></pre></div>'); 
-      el.find('pre').text(strprepad(++ilog,2, " ") + ': ' + s);
+      var el = $('<div><pre></pre></div>'); 
+      el.find('pre').addClass('log-item').text(strprepad(++ilog,2, " ") + ': ' + s);
       $(targetel).append(el);
     };
 })();

fd6d01eaf283c57fd09e12a202d2f4c761437e5e
Jon Davis <jon@jondavis.net>
2013-08-27 13:23:16 -0700

    Update log2el.js

--- log2el.js
+++ log2el.js
@@ -8,16 +8,23 @@
     }
     global.log2el = function(targetel, s) {
       var args = $.makeArray(arguments);
-      if (typeof(targetel) == 'string') {
-        args.unshift($('#log').length > 0 ? $('#log') : $('body'));
-      }
-      targetel = args[0];
-      s = args[1];
-      for (var i=2; i<args.length; i++) {
-          s = s.replace(new RegExp("\\{" + (i-2) + "\\}", 'g'), args[i]);
-      }
-      var el = $('<div><pre></pre></div>'); 
-      el.find('pre').addClass('log-item').text(strprepad(++ilog,2, " ") + ': ' + s);
-      $(targetel).append(el);
+      $(function() { // defer to document init
+        if (typeof(targetel) == 'string') {
+          args.unshift($('#log').length > 0 ? $('#log') : $(document.body));
+        }
+        targetel = args[0];
+        s = args[1];
+        for (var i=2; i<args.length; i++) {
+            s = s.replace(new RegExp("\\{" + (i-2) + "\\}", 'g'), args[i]);
+        }
+        var el = $('<div><pre></pre></div>'); 
+        var pre = el.find('pre');
+        pre.addClass('log-item');
+        if (ilog % 2 == 0) pre.addClass('alt');
+        pre.html('<span class="gutter">' 
+            + strprepad(++ilog,4, " ") + ': ' + '</span><span class="log-data"></span>');
+        el.find('.log-data').text(s);
+        $(targetel).append(el);
+      });
     };
 })();
